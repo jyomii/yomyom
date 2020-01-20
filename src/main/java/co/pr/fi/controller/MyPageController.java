9c2504e40f5e0b7c7131f9e844db2495b12a551f
user <user@GALFEE>
2020-01-20 09:01:03 +0900

    dd

--- src/main/java/co/pr/fi/controller/MyPageController.java
+++ src/main/java/co/pr/fi/controller/MyPageController.java
@@ -2,6 +2,8 @@ package co.pr.fi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import co.pr.fi.service.MyPageService;
 
@@ -10,4 +12,20 @@ public class MyPageController {
 
 	@Autowired
 	MyPageService myPageService;
+	
+	@RequestMapping(value="/mypage")
+	public String mypageMain() {
+		return "/mypage";
+	}
+	
+	@RequestMapping(value="/mypage2", method = RequestMethod.GET)
+	public String myedit() {
+		return "/mypage2";
+	}
+	
+	@RequestMapping(value="/mypage3", method = RequestMethod.GET)
+	public String myedit2() {
+		return "/mypage3";
+	}
+	
 }
