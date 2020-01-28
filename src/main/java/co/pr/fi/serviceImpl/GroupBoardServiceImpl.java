f012da7e245ce45713d835987154466a979da2f7
user1 <user1@DESKTOP-8UC660Q>
2020-01-28 18:49:38 +0900

    ㅇㅇㅇ

--- src/main/java/co/pr/fi/serviceImpl/GroupBoardServiceImpl.java
+++ src/main/java/co/pr/fi/serviceImpl/GroupBoardServiceImpl.java
@@ -1,11 +1,14 @@
 package co.pr.fi.serviceImpl;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import co.pr.fi.dao.GroupBoardDAO;
+import co.pr.fi.domain.Maps;
 import co.pr.fi.domain.Post;
 import co.pr.fi.service.GroupBoardService;
 
@@ -23,5 +26,25 @@ public class GroupBoardServiceImpl implements GroupBoardService {
 		}
 		System.out.println("조회수 증가 실패");
 		return null;
+	}
+
+	@Override
+	public int uploadMap(List<Maps> list2) {
+	
+		Map<String, Object> list = new HashMap<String, Object>();
+		list.put("list", list2);
+		return dao.uploadMap(list);
+	}
+
+	@Override
+	public List<Maps> getMap(int postkey) {
+	
+		return dao.getMap(postkey);
+	}
+
+	@Override
+	public int deleteAllMap() {
+		return dao.deleteAllMap();
+		
 	}
 }
